0c9f95d4c5c55bb04c61c7aba53cd5720ffb1a2a
kjh <kjhwert11@naver.com>
2020-03-15 08:57:23 +0900

    ## Router 적용

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import Card from "./js/Card";
+import Routes from "./js/Routes";
 
 function App() {
   return (
-    <Card/>
+    <Routes/>
   );
 }
 
---
+++ src/js/Player.js
@@ -0,0 +1,14 @@
+import React, {useState} from "react";
+import "../css/card.css";
+
+export default function Player() {
+
+    const [player1, setPlayer1] = useState('');
+    const [player2, setPlayer2] = useState('');
+
+    return (
+        <div className="container">
+
+        </div>
+    )
+}
