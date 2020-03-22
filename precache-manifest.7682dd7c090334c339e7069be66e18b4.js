ca7bde3e87957a8b617a7689c876254f76823496
kjh <kjhwert11@naver.com>
2020-03-22 15:29:40 +0900

    Updates

---
+++ precache-manifest.7682dd7c090334c339e7069be66e18b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "295f4db7f45b48bd8aa5d3ed11e0a522",
+    "url": "/index.html"
+  },
+  {
+    "revision": "983ab1b24ffea1fb27b1",
+    "url": "/static/css/main.d017e5ca.chunk.css"
+  },
+  {
+    "revision": "b54e62880da1307479b4",
+    "url": "/static/js/2.4635d382.chunk.js"
+  },
+  {
+    "revision": "e53f5d910d110e0950e9e282fd25563b",
+    "url": "/static/js/2.4635d382.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "983ab1b24ffea1fb27b1",
+    "url": "/static/js/main.6bd786be.chunk.js"
+  },
+  {
+    "revision": "828a053ec41466472180",
+    "url": "/static/js/runtime-main.21c5bba6.js"
+  },
+  {
+    "revision": "ae5a6f4469468ada24644dbf55c841c1",
+    "url": "/static/media/card_image02.ae5a6f44.png"
+  }
+]);
